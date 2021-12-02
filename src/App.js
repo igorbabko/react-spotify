052278b014092a47e3b2b29c294d3a7cbfefd0a6
Igor Babko <i.i.babko@studnote.com>
2021-12-02 12:19:37 +0200

    2: Add custom Tailwind CSS config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="underline text-[48px] text-[orange]">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-7xl font-black">Spotify Clone</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "500px",
+      md: "700px",
+      lg: "900px",
+      xl: "1100px",
+      "2xl": "1300px",
+      "3xl": "1500px",
+      "4xl": "1700px",
+      "5xl": "1900px",
+      "6xl": "2100px",
+    },
+    extend: {
+      gridTemplateColumns: {
+        "playlists-mobile": "repeat(auto-fill, minmax(208px, 1fr))",
+        "playlists-tablet": "repeat(auto-fill, minmax(266px, 1fr))",
+        "playlists-desktop": "repeat(auto-fill, minmax(180px, 1fr))",
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
