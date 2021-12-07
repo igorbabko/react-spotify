bf262979b0dff959e39a168172769dd0e3a04f8d
Igor Babko <i.i.babko@studnote.com>
2021-12-08 01:42:03 +0200

    classes

--- src/TheSidebar.js
+++ src/TheSidebar.js
@@ -2,12 +2,29 @@ import TheLogo from './TheLogo';
 import TheNav from './TheNav';
 import TheFooter from './TheFooter';
 
+const classes = [
+  'bg-black',
+  'w-[256px]',
+  'text-[#b2b2b2]',
+  'overflow-hidden',
+  'flex',
+  'flex-col',
+  'fixed',
+  'lg:sticky',
+  'top-0',
+  'z-30',
+  'h-screen',
+  'lg:h-auto',
+  '-translate-x-full',
+  'target:translate-x-0',
+  'lg:translate-x-0',
+  'transition-transform',
+  'peer',
+];
+
 function TheSidebar() {
   return (
-    <aside
-      id="sidebar"
-      className="bg-black w-[256px] text-[#b2b2b2] overflow-hidden flex flex-col fixed lg:sticky top-0 z-30 h-screen lg:h-auto -translate-x-full target:translate-x-0 lg:translate-x-0 transition-transform peer"
-    >
+    <aside id="sidebar" className={classes}>
       <TheLogo />
       <TheNav />
       <TheFooter />
