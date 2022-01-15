f9e0ac5403b3f258ab2287769e2f32cb7d7467b5
Igor Babko <i.i.babko@studnote.com>
2022-01-16 00:09:52 +0200

    27 - context menu timeout

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -25,6 +25,26 @@ const menuItems = [
   },
   {
     label: 'Open in Desktop app',
+    subMenuItems: [
+      {
+        label: 'Copy link to playlist',
+      },
+      {
+        label: 'Embed playlist',
+      },
+      {
+        label: 'Embed playlist 1',
+      },
+      {
+        label: 'Embed playlist 2',
+      },
+      {
+        label: 'Embed playlist 3',
+      },
+      {
+        label: 'Embed playlist 4',
+      },
+    ],
   },
 ];
 
