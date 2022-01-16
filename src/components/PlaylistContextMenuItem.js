a9ed7e26ffb3e92a8a40ba33c58a997b56c6bc2f
Igor Babko <i.i.babko@studnote.com>
2022-01-16 16:25:51 +0200

    26: Move context submenu left if no space to the right

--- src/components/PlaylistContextMenuItem.js
+++ src/components/PlaylistContextMenuItem.js
@@ -1,17 +1,48 @@
+import { useState, useRef } from 'react';
 import { ChevronRightIcon } from '@heroicons/react/outline';
 import PlaylistContextMenu from './PlaylistContextMenu';
 
 function PlaylistContextMenuItem({ children: label, subMenuItems }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [menuPositionClass, setMenuPositionClass] = useState('left-full');
+  const menuItemRef = useRef(null);
+
+  function getMenuPositionClass() {
+    const menuItem = menuItemRef.current;
+    const menuWidth = menuItem.offsetWidth;
+    const windowWidth = window.innerWidth;
+    const menuItemEndCoordinate = menuItem.getBoundingClientRect().right;
+    const shouldMoveMenuLeft = menuWidth > windowWidth - menuItemEndCoordinate;
+
+    return shouldMoveMenuLeft ? 'right-full' : 'left-full';
+  }
+
+  function openMenu() {
+    setIsMenuOpen(true);
+    setMenuPositionClass(getMenuPositionClass());
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   if (subMenuItems) {
     return (
-      <li className="relative">
-        <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center peer">
+      <li
+        className="relative"
+        onMouseEnter={openMenu}
+        onMouseLeave={closeMenu}
+        ref={menuItemRef}
+      >
+        <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center">
           {label} <ChevronRightIcon className="h-4 w-4" />
         </button>
-        <PlaylistContextMenu
-          menuItems={subMenuItems}
-          classes="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default invisible peer-hover:visible hover:visible"
-        />
+        {isMenuOpen && (
+          <PlaylistContextMenu
+            menuItems={subMenuItems}
+            classes={`bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default absolute top-0 ${menuPositionClass}`}
+          />
+        )}
       </li>
     );
   }
