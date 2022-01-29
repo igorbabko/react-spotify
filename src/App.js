006802c4d1180c63755eaf6d780e885fb98d18a2
Igor Babko <i.i.babko@studnote.com>
2022-01-29 11:47:20 +0200

    33 - update deps, line-clamp, tailwind.config.js, flex-grow class

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-grow overflow-auto">
+      <div className="flex grow overflow-auto">
         <TheSidebar />
         <TheSidebarOverlay />
         <div className="flex-1 overflow-auto" ref={contentWrapperRef}>
