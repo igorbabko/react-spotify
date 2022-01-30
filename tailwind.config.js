a0ad6a1f30adea00172aaaf5dd462bac313f0542
Igor Babko <i.i.babko@studnote.com>
2022-01-30 22:53:42 +0200

    33: Upgrade TailwindCSS from v2 to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     screens: {
       sm: "500px",
