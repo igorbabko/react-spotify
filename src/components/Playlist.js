f7bec681cde3a4c25ff612e7eb5791746e41ca74
Igor Babko <i.i.babko@studnote.com>
2022-05-21 15:03:19 +0300

    41: Extract specific behavior from useContextMenu hook

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
 import PlaylistCover from './PlaylistCover';
 import PlaylistButtonPlay from './PlaylistButtonPlay';
 import PlaylistTitle from './PlaylistTitle';
@@ -5,21 +6,73 @@ import PlaylistDescription from './PlaylistDescription';
 import PlaylistContextMenu from './PlaylistContextMenu';
 import useContextMenu from '../hooks/useContextMenu';
 
+function generateMenuItems(isAlternate = false) {
+  return [
+    {
+      label: 'Add to Your Library',
+    },
+    {
+      label: 'Share',
+      subMenuItems: [
+        {
+          label: isAlternate ? 'Copy Spotify URI' : 'Copy link to playlist',
+          classes: 'min-w-[150px]',
+        },
+        {
+          label: 'Embed playlist',
+        },
+      ],
+    },
+    {
+      label: 'About recommendations',
+    },
+    {
+      label: 'Open in Desktop app',
+    },
+  ];
+}
+
 function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
+  const [menuItems, setMenuItems] = useState(generateMenuItems());
+
   const {
-    bgClasses,
-    openContextMenu,
-    isContextMenuOpen,
-    contextMenuRef,
-    contextMenuItems,
-  } = useContextMenu(toggleScrolling);
+    openContextMenu: openMenu,
+    isContextMenuOpen: isMenuOpen,
+    contextMenuRef: menuRef,
+  } = useContextMenu();
+
+  useLayoutEffect(() => toggleScrolling(!isMenuOpen));
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    function handleAltKeydown({ key }) {
+      if (key === 'Alt') setMenuItems(generateMenuItems(true));
+    }
+
+    function handleAltKeyup({ key }) {
+      if (key === 'Alt') setMenuItems(generateMenuItems());
+    }
+
+    document.addEventListener('keydown', handleAltKeydown);
+    document.addEventListener('keyup', handleAltKeyup);
+
+    return () => {
+      document.removeEventListener('keydown', handleAltKeydown);
+      document.removeEventListener('keyup', handleAltKeyup);
+    };
+  });
+
+  const bgClasses = isMenuOpen
+    ? 'bg-[#272727]'
+    : 'bg-[#181818] hover:bg-[#272727]';
 
   return (
     <a
       href="/"
       className={`relative p-4 rounded-md duration-200 group ${classes} ${bgClasses}`}
       onClick={(event) => event.preventDefault()}
-      onContextMenu={openContextMenu}
+      onContextMenu={openMenu}
     >
       <div className="relative">
         <PlaylistCover url={coverUrl} />
@@ -27,10 +80,10 @@ function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
       </div>
       <PlaylistTitle title={title} />
       <PlaylistDescription description={description} />
-      {isContextMenuOpen && (
+      {isMenuOpen && (
         <PlaylistContextMenu
-          ref={contextMenuRef}
-          menuItems={contextMenuItems}
+          ref={menuRef}
+          menuItems={menuItems}
           classes="fixed divide-y divide-[#3e3e3e]"
         />
       )}
