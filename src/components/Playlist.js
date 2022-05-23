596c29cd5bd07f5264262e65e15f042c3b90c360
Igor Babko <i.i.babko@studnote.com>
2022-05-23 09:42:47 +0300

    43: Refactor useContextMenu hook

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,7 +4,7 @@ import PlaylistButtonPlay from './PlaylistButtonPlay';
 import PlaylistTitle from './PlaylistTitle';
 import PlaylistDescription from './PlaylistDescription';
 import PlaylistContextMenu from './PlaylistContextMenu';
-import useContextMenu from '../hooks/useContextMenu';
+import useMenu from '../hooks/useContextMenu';
 
 function generateMenuItems(isAlternate = false) {
   return [
@@ -33,18 +33,13 @@ function generateMenuItems(isAlternate = false) {
 }
 
 function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
-  const [menuItems, setMenuItems] = useState(generateMenuItems());
+  const [menuItems, setMenuItems] = useState(generateMenuItems);
+  const menu = useMenu(menuItems);
 
-  const {
-    openContextMenu: openMenu,
-    isContextMenuOpen: isMenuOpen,
-    contextMenuRef: menuRef,
-  } = useContextMenu();
-
-  useLayoutEffect(() => toggleScrolling(!isMenuOpen));
+  useLayoutEffect(() => toggleScrolling(!menu.isOpen));
 
   useEffect(() => {
-    if (!isMenuOpen) return;
+    if (!menu.isOpen) return;
 
     function handleAltKeydown({ key }) {
       if (key === 'Alt') setMenuItems(generateMenuItems(true));
@@ -63,7 +58,7 @@ function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
     };
   });
 
-  const bgClasses = isMenuOpen
+  const bgClasses = menu.isOpen
     ? 'bg-[#272727]'
     : 'bg-[#181818] hover:bg-[#272727]';
 
@@ -72,7 +67,7 @@ function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
       href="/"
       className={`relative p-4 rounded-md duration-200 group ${classes} ${bgClasses}`}
       onClick={(event) => event.preventDefault()}
-      onContextMenu={openMenu}
+      onContextMenu={menu.open}
     >
       <div className="relative">
         <PlaylistCover url={coverUrl} />
@@ -80,10 +75,10 @@ function Playlist({ classes, coverUrl, title, description, toggleScrolling }) {
       </div>
       <PlaylistTitle title={title} />
       <PlaylistDescription description={description} />
-      {isMenuOpen && (
+      {menu.isOpen && (
         <PlaylistContextMenu
-          ref={menuRef}
-          menuItems={menuItems}
+          ref={menu.ref}
+          menuItems={menu.items}
           classes="fixed divide-y divide-[#3e3e3e]"
         />
       )}
