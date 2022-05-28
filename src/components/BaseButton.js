196a05a189f6d2eb2ca7c32582778d7d0b7ccc4a
Igor Babko <i.i.babko@studnote.com>
2022-05-28 21:37:29 +0300

    53: Hide popover

--- src/components/BaseButton.js
+++ src/components/BaseButton.js
@@ -1,4 +1,9 @@
-function BaseButton({ primary, classes, children: label }) {
+function BaseButton({
+  primary,
+  classes,
+  onClick: handleClick,
+  children: label,
+}) {
   const typeClasses = primary
     ? 'bg-white hover:bg-gray-100 text-[#2e2e2e]'
     : 'text-white';
@@ -6,6 +11,7 @@ function BaseButton({ primary, classes, children: label }) {
   return (
     <button
       className={`font-semibold leading-5 py-[9px] px-[17px] sm:px-[38px] rounded-full hover:scale-105 ${typeClasses} ${classes}`}
+      onClick={handleClick}
     >
       {label}
     </button>
--- src/components/BasePopover.js
+++ src/components/BasePopover.js
@@ -1,12 +1,33 @@
+import { useState, useEffect, useRef } from 'react';
 import BaseButton from './BaseButton';
 
 function BasePopover() {
+  const [classes, setClasses] = useState();
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClickAway({ target }) {
+      if (!ref.current.contains(target)) hide();
+    }
+
+    document.addEventListener('mousedown', handleClickAway);
+
+    return () => document.removeEventListener('mousedown', handleClickAway);
+  });
+
+  function hide() {
+    setClasses('opacity-0 pointer-events-none');
+  }
+
   return (
-    <div className="fixed top-[227px] left-[200px] z-30 bg-[#0e72ea] text-white tracking-wide rounded-lg shadow-3xl p-4 min-w-[330px] select-none">
+    <div
+      className={`fixed top-[227px] left-[200px] z-30 bg-[#0e72ea] text-white tracking-wide rounded-lg shadow-3xl p-4 min-w-[330px] select-none transition duration-300 ${classes}`}
+      ref={ref}
+    >
       <h3 className="text-lg font-bold mb-2">Create a playlist</h3>
       <p className="text-xs">Log in to create and share playlists.</p>
       <div className="mt-6 text-right">
-        <BaseButton>Not now</BaseButton>
+        <BaseButton onClick={hide}>Not now</BaseButton>
         <BaseButton primary>Log in</BaseButton>
       </div>
       <div className="w-20 h-20 absolute -top-4 -left-20 flex justify-end items-center overflow-hidden pointer-events-none">
