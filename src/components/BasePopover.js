aa28619982709f7297a1f5bf95a95ea4077f208a
Igor Babko <i.i.babko@studnote.com>
2022-05-28 19:15:43 +0300

    51 - popover triangle

--- src/components/BasePopover.js
+++ src/components/BasePopover.js
@@ -10,6 +10,9 @@ function BasePopover() {
         <TheButtonRegister />
         <TheButtonLogin />
       </div>
+      <div className="w-20 h-20 absolute -top-4 -left-20 flex justify-end items-center overflow-hidden pointer-events-none">
+        <div className="w-3 h-3 bg-[#0e72ea] translate-x-1/2 rotate-45 shadow-3xl pointer-events-auto" />
+      </div>
     </div>
   );
 }
