8c666edd9538536b828e6594116ffed55ff4c069
Igor Babko <i.i.babko@studnote.com>
2022-05-28 19:07:12 +0300

    53 - hide popover

--- src/components/BasePopover.js
+++ src/components/BasePopover.js
@@ -1,16 +1,37 @@
+import { useState, useEffect, useRef } from 'react';
 import BaseButton from './BaseButton';
 
 function BasePopover() {
+  const [classes, setClasses] = useState();
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClickAway({ target }) {
+      if (!ref.current.contains(target)) hide();
+    }
+
+    document.addEventListener('mousedown', handleClickAway);
+
+    return () => document.removeEventListener('mousedown', handleClickAway);
+  });
+
+  function hide() {
+    setClasses('opacity-0 pointer-events-none');
+  }
+
   return (
-    <div className="fixed top-[227px] left-[200px] z-30 bg-[#0e72ea] text-white tracking-wide rounded-lg shadow-3xl p-4 min-w-[330px] select-none transition duration-300 opacity-1 -translate-x-1">
+    <div
+      className={`fixed top-[227px] left-[215px] z-30 bg-[#0e72ea] text-white tracking-wide rounded-lg shadow-3xl p-4 min-w-[330px] select-none transition duration-300 ${classes}`}
+      ref={ref}
+    >
       <h3 className="text-lg font-bold mb-2">Create a playlist</h3>
       <p className="text-xs">Log in to create and share playlists.</p>
       <div className="mt-6 text-right">
-        <BaseButton>Not now</BaseButton>
+        <BaseButton onClick={hide}>Not now</BaseButton>
         <BaseButton primary>Log in</BaseButton>
       </div>
       <div className="w-20 h-20 absolute -top-4 -left-20 flex justify-end items-center overflow-hidden pointer-events-none">
-        <div className="w-3 h-3 bg-[#0e72ea] translate-x-1/2 rotate-45 shadow-3xl pointer-events-auto" />
+        <div className="w-3 h-3 bg-[#0e72ea] translate-x-1/2 rotate-45 shadow-3xl pointer-events-auto"></div>
       </div>
     </div>
   );
