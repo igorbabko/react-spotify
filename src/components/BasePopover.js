d09e8819859013d7f662dbddd767d316e1f79a0b
Igor Babko <i.i.babko@gmail.com>
2022-06-16 12:12:14 +0300

    72: Move resizing logic to usePopoverPosition hook

--- src/components/BasePopover.js
+++ src/components/BasePopover.js
@@ -1,4 +1,3 @@
-import { MIN_DESKTOP_WIDTH, debounce } from '../utils';
 import {
   useState,
   useEffect,
@@ -10,53 +9,23 @@ import usePosition from '../hooks/usePopoverPosition';
 import BaseButton from './BaseButton';
 import BasePopoverTriangle from './BasePopoverTriangle';
 
-function isCurrentWindowWidthSmall() {
-  return window.innerWidth < MIN_DESKTOP_WIDTH;
-}
-
-function isCurrentWindowWidthBig() {
-  return window.innerWidth >= MIN_DESKTOP_WIDTH;
-}
-
 function BasePopover(_, ref) {
-  const [isSmallScreen, setIsSmallScreen] = useState(isCurrentWindowWidthSmall);
-  const [classes, setClasses] = useState(getHiddenClasses);
-  const [target, setTarget] = useState();
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
   const nodeRef = useRef();
-  const changeWidthTimer = useRef();
-  const move = usePosition(nodeRef, isSmallScreen);
+  const { move, target, setTarget, isSmallScreen } = usePosition(nodeRef, hide);
+  const [classes, setClasses] = useState(getHiddenClasses);
 
   useEffect(() => {
-    function handleResize() {
-      if (!screenHasBecomeSmall() && !screenHasBecomeBig()) return;
-
-      hide();
-
-      clearTimeout(changeWidthTimer.current);
-
-      changeWidthTimer.current = setTimeout(
-        () => setIsSmallScreen(isCurrentWindowWidthSmall),
-        300
-      );
-    }
-
     function handleClickAway(event) {
       if (target && target.parentNode.contains(event.target)) return;
 
       if (!nodeRef.current.contains(event.target)) hide();
     }
 
-    const debounceResize = debounce.bind(null, handleResize, 300);
-
-    window.addEventListener('resize', debounceResize);
     document.addEventListener('mousedown', handleClickAway);
 
-    return () => {
-      window.removeEventListener('resize', debounceResize);
-      document.removeEventListener('mousedown', handleClickAway);
-    };
+    return () => document.removeEventListener('mousedown', handleClickAway);
   });
 
   useImperativeHandle(ref, () => ({ show }));
@@ -65,7 +34,6 @@ function BasePopover(_, ref) {
     if (target === nextTarget) return;
 
     move(nextTarget, offset);
-    setTarget(nextTarget);
     setTitle(title);
     setDescription(description);
     setClasses('');
@@ -80,14 +48,6 @@ function BasePopover(_, ref) {
     const translateClass = isSmallScreen ? 'translate-y-1' : 'translate-x-1';
 
     return `opacity-0 ${translateClass} pointer-events-none`;
-  }
-
-  function screenHasBecomeSmall() {
-    return isCurrentWindowWidthSmall() && !isSmallScreen;
-  }
-
-  function screenHasBecomeBig() {
-    return isCurrentWindowWidthBig() && isSmallScreen;
   }
 
   return (
